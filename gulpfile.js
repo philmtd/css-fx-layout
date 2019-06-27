e0dec52a2648c3bd6d87f231e651626a3e8bb404
Philip Dakowitz <philip.dakowitz@dakowitz.de>
2019-06-27 22:49:52 +0200

    Hello world. Initial version of everything.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+sass.compiler = require('node-sass');
+
+var sassFiles = './src/**/*.scss';
+var cssOut = 'out';
+
+gulp.task('sass', function() {
+    return gulp.src('src/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest(cssOut));
+});
+
+gulp.task('sass:watch', function() {
+   gulp.watch(sassFiles, {ignoreInitial: false}, gulp.series('sass'));
+});
