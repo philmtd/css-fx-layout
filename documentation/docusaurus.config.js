449017f271a5a62659373695ee3d6c1bb744a141
Philip Dakowitz <philip.dakowitz@dakowitz.de>
2023-02-20 23:43:33 +0100

    add actions config

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -20,9 +20,12 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'philmtd', // Usually your GitHub org/user name.
   projectName: 'css-fx-layout', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
