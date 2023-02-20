1acb8ccc627422f74fd49a275748fddbbcbf7d58
Philip Dakowitz <philip.dakowitz@dakowitz.de>
2023-02-20 22:56:44 +0100

    freeze docs for v2.1

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://philmtd.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/css-fx-layout",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
