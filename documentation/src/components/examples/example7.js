efc78caccf73e100ae8c8cdab2843d9967e3d17d
Philip Dakowitz <philip.dakowitz@dakowitz.de>
2023-02-27 16:56:18 +0100

    docs: remove unused css class from example

--- documentation/src/components/examples/example7.js
+++ documentation/src/components/examples/example7.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const attributesCode = `<div data-layout="row">
   <span data-flex-xs data-flex-md data-flex-xl>One</span>
-  <span class="green" data-flex-sm data-flex-lg>Two</span>
+  <span data-flex-sm data-flex-lg>Two</span>
 </div>`;
 
 const classesCode = `<div class="fx-layout-row">
   <span class="fx-flex--xs fx-flex--md fx-flex--xl">One</span>
-  <span class="green fx-flex--sm fx-flex--lg">Two</span>
+  <span class="fx-flex--sm fx-flex--lg">Two</span>
 </div>`;
 
 export default {
